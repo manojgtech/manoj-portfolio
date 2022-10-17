398f82deb817c9b338696ec1cd3e73251a32bc2c
manoj kumar <mkmkgupta42@gmail.com>
2022-10-17 21:21:21 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import myimg from './img/me.jpg';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className='row'>
+        <div className='col-md-2'>
+        
+        <nav className="navbar navbar-dark">
+          
+          <div className="container-fluid leftdiv">
+          <Link to='/' className="navbar-brand">
+          <img src={myimg} alt="Avatar Logo"  className="rounded-pill myinglogo" /> 
+           </Link>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About</Link>
+            </li>
+          </ul>
+        </div>
+</nav>
+
+
+        </div>
+        <div className='col-md-8'>
+        
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+        
+    </Routes>
+  
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='section-home text-center'>
+            <h3>Hello <span className='text-primary'>I am</span></h3>
+            <h1 className='text-info'>Manoj Kumar Gupta</h1>
+            <h3>Professional Software Developer</h3>
+            <p>
+                I have more then 6 year exeperience in web developemnt on <br/> variaus plateforms.I have worked on fronend and backend <br/> technologies.
+            </p>
+        </div>
+    );
+}
+
+export default Home;
