693a1a0e419f2a05c176347e668221d63968f980
manoj kumar <mkmkgupta42@gmail.com>
2022-11-17 22:21:36 +0530

    gfgg

---
+++ src/components/Resume.js
@@ -0,0 +1,124 @@
+import React from 'react';
+
+
+const Resume = () => {
+return(
+<section id="resume" className="resume">
+  <div className="container aos-init aos-animate" data-aos="fade-up">
+    <div className="section-title">
+      <h2>Resume</h2>
+      <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+    </div>
+    <div className="row">
+      <div className="col-lg-6">
+        <h3 className="resume-title">Sumary</h3>
+        <div className="resume-item pb-0">
+          <h4>Manoj Kumar Gupta</h4>
+          <p>
+            <em>Innovative and deadline-driven Fullstack Developer with 5+ years of experience developing e-commerce and .</em>
+          </p>
+          <p></p>
+          <ul>
+            <li>Vandna enclave,khora colony Noida</li>
+            <li>+91 8447311900</li>
+            <li>mkmkgupta42@gmail.com</li>
+          </ul>
+          <p></p>
+        </div>
+        <h3 className="resume-title">Education</h3>
+        <div className="resume-item">
+          <h4>Bachelor of Technology &amp; Computer Science</h4>
+          <h5>2009 - 2013</h5>
+          <p>
+            <em>APJ Abdul Kalam Technical University,Lucknow</em>
+          </p>
+         
+        </div>
+        
+      </div>
+      <div className="col-lg-6">
+        <h3 className="resume-title">Professional Experience</h3>
+        <div className="resume-item">
+          <h4>Senior Software Developer/h4>
+          <h5>Oct 2020 - Present</h5>
+          <p>
+            <em>Clavis Technology, Noida,UP </em>
+          </p>
+          <p></p>
+          {/* <ul>
+            <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
+            <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project.</li>
+            <li>Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design</li>
+            <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000</li>
+          </ul> */}
+          <p></p>
+        </div>
+        <div className="resume-item">
+          <h4>Web Developer</h4>
+          <h5>March 2020 - Sep 2020</h5>
+          <p>
+            <em>Veom Infotech, Noida</em>
+          </p>
+          <p></p>
+          {/* <ul>
+            <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
+            <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
+            <li>Recommended and consulted with clients on the most appropriate graphic design</li>
+            <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
+          </ul> */}
+          <p></p>
+        </div>
+        <div className="resume-item">
+          <h4>Web Developer</h4>
+          <h5>July 2018 - Dec 2019</h5>
+          <p>
+            <em>KVR Web Technology, Panchkula</em>
+          </p>
+          <p></p>
+          {/* <ul>
+            <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
+            <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
+            <li>Recommended and consulted with clients on the most appropriate graphic design</li>
+            <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
+          </ul> */}
+          <p></p>
+        </div>
+        <div className="resume-item">
+          <h4>Web Developer Trainee</h4>
+          <h5>July 2014 - Dec 2014</h5>
+          <p>
+            <em>ASMMD Infosytem, New Delhi</em>
+          </p>
+          <p></p>
+          {/* <ul>
+            <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
+            <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
+            <li>Recommended and consulted with clients on the most appropriate graphic design</li>
+            <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
+          </ul> */}
+          
+          <p></p>
+        </div>
+        <div className="resume-item">
+          <h4>Web Developer</h4>
+          <h5>March 2020 - Sep 2020</h5>
+          <p>
+            <em>Veom Infotech, Noida</em>
+          </p>
+          <p></p>
+          {/* <ul>
+            <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
+            <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
+            <li>Recommended and consulted with clients on the most appropriate graphic design</li>
+            <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
+          </ul> */}
+          <p></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+);
+}
+
+export default Resume;
